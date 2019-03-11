5cb95f86e32f84f3320ca3dda29de8ae7f968db7
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2019-03-11 14:58:08 -0400

    Add files via upload

--- source/core/Droplet.js
+++ source/core/Droplet.js
@@ -2,31 +2,36 @@
 class Droplet {
 
 	// Construct a new droplet with starting x / y, a rate of fall, and density.
-	constructor(startx, starty, rate, density) {
+	constructor(startx, starty, rate, density, deathHeight) {
 		this.x = startx;
 		this.y = starty;
 		this.v = rate;
 		this.d = density;
+		this.isDead = false;
+		this.dh = deathHeight;
 	}
 
 
 
 	// Update the position of the droplet
 	update() {
-		this.y += v;
+		this.y += this.v;
+		if(this.y > this.dh) {
+			this.isDead = true;
+		}
 	}
 
 	// Draw to canvas
 	draw() {
 
 		// Reference our canvas and its context
+
 		var canv = document.getElementById("rainCanv");
 		var ctx = canv.getContext("2d");
-
 		// begin drawing
 		ctx.beginPath();
 		ctx.arc(this.x, this.y, this.d, 0, Math.PI * 2);
-		ctx.fillStyle = "#ffffff";
+		ctx.fillStyle = "#6b98e0";
 		ctx.fill();
 		ctx.closePath();
 		
--- source/core/Rain.js
+++ source/core/Rain.js
@@ -5,6 +5,10 @@ var interval;
 
 var droplets = [];
 
+// Population size of droplets
+var size = 1500;
+
+
 function rainInit() {
 
 	// Set up the canvas.
@@ -15,31 +19,52 @@ function rainInit() {
 	ctx.canvas.width = window.innerWidth - 10;
 	ctx.canvas.height = window.innerHeight - 10;
 
-	// Create a variable containing an interval that runs the rain code. We place it in a variable so we can clear the interval whenever the user presses start.
+	// Create a variable containing an interval that runs the rain code. We place it in a variable so we can clear the interval whenever the user presses start. Runs at 60 frames.
 	interval = setInterval(function() {
 		rainDroplets();
-	}, 1000 / 30);
+	}, 1000 / 60);
 
 }
 
 function rainDroplets() {
 
-	for(var i = 0; i < 100; i++) {
-		if(droplets.length > 100) {
+	// I could've created global vars but meh, this will do
+	var canvas = document.getElementById("rainCanv");
+	var cont = canvas.getContext("2d");
+
+	// Clear every frame
+	cont.clearRect(0, 0, canvas.width, canvas.height);
+
+	// Populate array upon first run.
+	for(var i = 0; i < size; i++) {
+		if(droplets.length > size) {
 			break;
 		}
-		droplets.push(new Droplet(returnRandom(5, window.innerWidth - 5), returnRandom(5, window.innerHeight - 5), returnRandom(1, 3), returnRandom(3, 5)));
+		// We randomize death height to create a natural removal of the rain droplets.
+		droplets.push(new Droplet(returnRandom(1, window.innerWidth - 5), returnRandom(1, window.innerHeight - 10), returnRandom(4, 9), 1, window.innerHeight - returnRandom(1, 10)));
 	}
 
-	for(var i = 0; i < 100; i++) {
+	/* First check if the droplet is "dead", basically off of the screen. If it is, we're going to replace it's value with a new droplet that starts between 1,30
+	* Note that in the first class definition, we define within the bounds of the entire window and in the second we define 1,30. This is because we want it to start
+	* naturally, and then keep spawning from the top. This prevents any gaps.
+	*/
+	for(var i = 0; i < size; i++) {
 		var drop = droplets[i];
-		drop.update();
-		drop.draw();
-	}
 
+		if(drop.isDead) {
+			// Replace value with new droplet starting at top
+			droplets[i] = new Droplet(returnRandom(1, window.innerWidth - 5), returnRandom(1, 30), returnRandom(4, 9), 1, window.innerHeight - returnRandom(1, 10));
+		} else {
+			// Update then draw
+			drop.update();
+			drop.draw();
+		}
+	}
+	
 
 }
 
+// Just a function to return a random int
 function returnRandom(min, max) {
     return Math.floor(Math.random() * max) + min;
 }
