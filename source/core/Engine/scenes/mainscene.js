71a1f4efcd3db5fd016797a053935996bf5f1406
Austin <38254442+SpecifiesDev@users.noreply.github.com>
2019-03-13 14:54:11 -0400

    Renamed

--- source/core/Engine/scenes/mainscene.js
+++
@@ -1,8 +0,0 @@
-// mainscene.js
-
-function setScene() {
-
-	window.scene = new THREE.Scene();
-	window.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1e27);
-
-}
